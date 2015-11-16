bbe550da69d93646c203f55d77b5f95deb7608ce
Daniel Vella <danielvella21@gmail.com>
2015-11-16 19:12:03 +0100

    Fresh Installation of Laravel (Sort Of)
    - A fresh installation of laravel framework (sort of)

---
+++ app/Maker.php
@@ -0,0 +1,17 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Maker extends Model {
+
+	protected $table = 'makers';
+
+	protected $fillable = ['name', 'phone'];
+
+	protected $hidden = ['id', 'created_at', 'updated_at'];
+
+	public function vehicle() {
+		return $this->hasMany('App\Vehicle');
+	}
+
+}
